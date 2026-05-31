5f850d61e6e1c5ea5967642aa1e340588ed92754
kayla133 <makaylahansen04@gmail.com>
2026-05-30 19:53:01 -0600

    user registration system

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -4,6 +4,7 @@ import { catalogPage, courseDetailPage } from './catalog/catalog.js';
 import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
 import { facultyPage, facultyDetailPage } from './faculty/faculty.js';
 import contactRoutes from './forms/contact.js';
+import registrationRoutes from './forms/registration.js';
 
 const router = Router();
 
@@ -30,5 +31,13 @@ router.get('/demo', addDemoHeaders, demoPage);
 router.get('/test-error', testErrorPage);
 // Contact form routes
 router.use('/contact', contactRoutes);
+
+// Add registration-specific styles to all registration routes
+router.use('/register', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/registration.css">');
+    next();
+});
+// Registration routes
+router.use('/register', registrationRoutes);
 
 export default router;
